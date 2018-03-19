3f1c8afa5e346496d4eb5d81b119e9cd8b83a14b
bragamat <mateuscbraga@gmail.com>
2018-03-19 18:05:54 +0000

    reverse! funny one :D

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,8 +1,15 @@
 var https = require('https');
 
-var getHTML = require('./4thstep.js');
+var getHTML = require('../4thstep.js');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step6/reverse.html'
 };
+
+function reverseIt(html) {
+  console.log(html.split('').reverse().join(''));
+}
+
+
+getHTML(requestOptions, reverseIt);
