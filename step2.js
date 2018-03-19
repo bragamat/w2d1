b61322b7ac45e3b4e697ebd01dce78874a2b34e3
bragamat <mateuscbraga@gmail.com>
2018-03-19 15:41:09 +0000

    2nd step followed

---
+++ step2.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+  var buffer = "";
+  /* Add your code here */
+  https.get(requestOptions, function(res) {
+    
+    res.setEncoding('utf8');
+    
+    res.on("data" , function(chunk) {
+      console.log(chunk);
+    buffer += chunk;
+    }) 
+  }); 
+}
+
+getAndPrintHTML();
